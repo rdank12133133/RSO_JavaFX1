c912668b4e79f1354c71e2fd7ab66efb748e44a8
MD20M <maj.donko@gmail.com>
2023-09-29 12:24:14 +0200

    Commit

---
+++ src/main/java/com/example/rso_java_majdonko/HelloApplication.java
@@ -0,0 +1,21 @@
+package com.example.rso_java_majdonko;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Group root = new Group();
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
